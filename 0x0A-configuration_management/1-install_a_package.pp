93f8c2c2ec6be7fbed89d43c47d3aad1703bad19
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-23 13:00:54 -0800

    ..

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,6 +5,11 @@ package { 'python3.8':
   ensure => present,
 }
 
+# ensure that pip present
+package { 'python3-pip':
+  ensure => present,
+}
+
 # Install Flask 
 package { 'flask':
   ensure   => '2.1.0',
