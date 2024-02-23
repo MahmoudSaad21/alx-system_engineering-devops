35634189cfe7d896ace2eaa19596664613cec946
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-23 12:54:35 -0800

    ..

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
 # install_flask.pp
 
+# ensure that python present
 package { 'python3-pip':
   ensure => installed,
 }
 
+# install flask
 exec { 'install_flask':
   command     => '/usr/bin/pip3 install flask==2.1.0',
   path        => ['/usr/bin', '/usr/local/bin'],
